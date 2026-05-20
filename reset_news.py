a8efb860213208166db25f115b203374342b9ff4
kepler45-2b <wallislevi@gmail.com>
2026-05-20 09:37:49 +0200

    Delete reset_news.py

--- reset_news.py
+++
@@ -1,7 +0,0 @@
-import chromadb
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-db = chromadb.PersistentClient(path=os.path.join(BASE_DIR, "data/chroma"))
-db.delete_collection("news")
-print("News collection deleted.")
